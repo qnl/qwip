7ebccdc723f411f58ce16e5d71a6a2354b2dba09
Larry Chen <larrychen@berkeley.edu>
2021-10-31 17:15:01 -0700

    Replaced functools.py w/ typedispatch decorator

--- src/qwip/settings/serialization.py
+++ src/qwip/settings/serialization.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from loguru import logger
 
+from functools import singledispatch, update_wrapper
+
 from pendulum import Date
 from enum import Enum
 from collections.abc import Mapping
@@ -12,29 +14,66 @@ from numbers import Number
 
 from qwip.settings.base import SettingsBase
 from qwip.settings.typing import get_origin, get_args
-from qwip.settings.functools import singledispatch
 from qwip.settings.parameters import Parameters
 
-@singledispatch
+def typedispatch(func):
+    """Type-dispatch generic function decorator.
+
+    This function modifies the singledispatch function from the functools module
+    to allow for single dispatch converters based on types. This allows converters
+    to be defined for different attributes when attrs classes are being created.
+    """
+
+    dispatcher = singledispatch(func)
+    
+    def wrapper(*args, **kwargs):
+        if not args:
+            raise TypeError(f'{funcname} requires at least '
+                            '1 positional argument')
+        return dispatcher.dispatch(args[0])(*args, **kwargs)
+    
+    funcname = getattr(func, '__name__', 'typedispatch function')
+    wrapper.register = dispatcher.register
+    wrapper.dispatcher = dispatcher
+    update_wrapper(wrapper, func)
+    return wrapper
+
+@typedispatch
 def structure(field_type, field):
+    """Generic type based single dispatch converter for settings.
+
+    Args:
+        field_type (type): The field type.
+        field: The fie
+    """
     logger.warning(
-        f'No registered structure method for {field.name} of type {type(field_type).__name__}')
+        f'No registered structure method for field "{field.name}" of type {field_type.__name__}'
+    )
     return None
 
 @structure.register(bool)
 @structure.register(str)
 @structure.register(Number)
 def _(field_type, field):
+    logger.debug(
+        f'Creating null converter for field "{field.name}" of type {field_type.__name__}'
+    )
     return None
 
-@structure.register
-def _(field_type: Enum, field):
+@structure.register(Enum)
+def _(field_type, field):
+    logger.debug(
+        f'Creating Enum converter for field "{field.name}" of type {field_type.__name__}'
+    )
     def _structure(maybe_str):
         return field_type(maybe_str)
     return _structure
 
-@structure.register
-def _(field_type: list, field):
+@structure.register(list)
+def _(field_type, field):
+    logger.debug(
+        f'Creating list converter for field "{field.name}" of type {field_type.__name__}'
+    )
     element_type = get_args(field.type)[0]
     structure_element = structure(element_type, field)
     def _structure(lst):
@@ -44,8 +83,11 @@ def _(field_type: list, field):
         return lst
     return _structure
 
-@structure.register
-def _(field_type: Mapping, field):
+@structure.register(Mapping)
+def _(field_type, field):
+    logger.debug(
+        f'Creating mapping converter for field "{field.name}" of type {field_type.__name__}'
+    )
     value_type = get_args(field.type)[1]
     structure_value = structure(value_type, field)
     def _structure(d):
@@ -59,14 +101,20 @@ def _(field_type: Mapping, field):
         })
     return _structure
 
-@structure.register
-def _(field_type: SettingsBase, field):
+@structure.register(SettingsBase)
+def _(field_type, field):
+    logger.debug(
+        f'Creating Settings converter for field "{field.name}" of type {field_type.__name__}'
+    )
     def _structure(maybe_dict):
         return field_type(**maybe_dict)
     return _structure
 
-@structure.register
-def _(field_type: Date, field):
+@structure.register(Date)
+def _(field_type: type, field: Date):
+    logger.debug(
+        f'Creating Date converter for field "{field.name}" of type {field_type.__name__}'
+    )
     def _structure(maybe_str):
         if isinstance(maybe_str, str):
             return pendulum.parse(maybe_str).date()
@@ -74,8 +122,11 @@ def _(field_type: Date, field):
             return maybe_str
     return _structure
 
-@structure.register
-def _(field_type:np.ndarray, field):
+@structure.register(np.ndarray)
+def _(field_type, field):
+    logger.debug(
+        f'Creating ndarray converter for field "{field.name}" of type {field_type.__name__}'
+    )
     def _structure(maybe_ndarray):
         if isinstance(maybe_ndarray, np.ndarray):
             return maybe_ndarray
