644d55ea06a25feb8520a102ecac36913a338717
Larry Chen <larrychen@berkeley.edu>
2022-11-28 01:55:01 -0800

    Catch exceptions on attempted convert and leave value unconverted

--- src/qwip/settings/serialization.py
+++ src/qwip/settings/serialization.py
@@ -1,16 +1,29 @@
 """Serializers for settings."""
 
+from loguru import logger
+
 import qwip
+
+def get_type_converter(cls):
+    def convert(v):
+        """Attempts type structuring.
+        
+        We want most errors to be caught by validation since this provides a
+        clearer error message"""
+        try:
+            return qwip.converter.structure(v, cls)
+        except Exception as e:
+            logger.debug(f'Conversion failed due to {e}.')
+            return v
+
+    return convert
 
 def add_type_converters(cls, fields):
     new_fields = []
 
     for field in fields:
-        def type_converter(cls):
-            return lambda v: qwip.converter.structure(v, cls)
-
         if field.metadata.get('auto_convert', True) and field.init:
-            field = field.evolve(converter=type_converter(field.type))
+            field = field.evolve(converter=get_type_converter(field.type))
 
         new_fields.append(field)
 
