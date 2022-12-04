7f0a823d9bcebe3150003d962cc1d490d7fd7b5a
Larry Chen <larrychen@berkeley.edu>
2022-12-03 16:31:09 -0800

    Added scalar multiplication to location

--- src/qwip/sequencer/locations.py
+++ src/qwip/sequencer/locations.py
@@ -1,4 +1,5 @@
 from numbers import Real
+from typing import ClassVar
 
 import attrs
 from attrs import field, resolve_types
@@ -7,6 +8,9 @@ from qwip.settings.settings import qfrozen
 from qwip.flatdict import FlatDict
 from qwip.settings.validation import resolve_types_with_validation
 
+
+def _type_error_text(op1, op2, operand: str) -> str:
+    return f'unsupported operand type(s) for {operand}: {type(op1)!r} and {type(op2)!r}'
 
 @qfrozen(kw_only=False)
 class Location:
@@ -24,9 +28,7 @@ class Location:
     def __add__(self, other) -> 'Location':
         """Adds two locations."""
         if not isinstance(other, (Location, Real)):
-            raise TypeError(
-                f'unsupported operand type(s) for +: {type(self)!r} and {type(other)!r}'
-            )
+            raise TypeError(_type_error_text(self, other, '+'))
 
         if isinstance(other, Real):
             return Location(
@@ -35,7 +37,7 @@ class Location:
 
         coeff_map = {loc: c for loc, c in self.references}
         
-        for c, loc in other.references:
+        for loc, c in other.references:
             coeff_map[loc] = coeff_map.get(loc, 0) + c
 
         return Location(
@@ -47,28 +49,49 @@ class Location:
 
     def __radd__(self, other) -> 'Location':
         """Adds two locations."""
-        if not isinstance(other, (Location, Real)):
-            raise TypeError(
-                f'unsupported operand type(s) for +=: {type(self)!r} and {type(other)!r}'
-            )
         return self.__add__(other)
 
-    def __sub__(self, other_location) -> 'Location':
+    def __sub__(self, other) -> 'Location':
         """Subtracts two locations."""
+        if not isinstance(other, (Location, Real)):
+            raise TypeError(_type_error_text(self, other, '-'))
+
+        if isinstance(other, Real):
+            return Location(
+                offset=self.offset - other, references=self.references
+            )
+
         coeff_map = {loc: c for loc, c in self.references}
 
-        for c, loc in other_location.references:
+        for loc, c in other.references:
             coeff_map[loc] = coeff_map.get(loc, 0) - c
 
         return Location(
-            offset=self.offset - other_location.offset,
+            offset=self.offset - other.offset,
             references=frozenset(
                 (loc, c) for loc, c in coeff_map.items() if c != 0
             )
         )
     
-    def __rsub__(self, other_location) -> 'Location':
+    def __rsub__(self, other) -> 'Location':
         """Subtracts two locations."""
-        return self.__sub__(other_location)
+        return -self.__sub__(other)
+
+    def __mul__(self, other) -> 'Location':
+        """Scalar multiplication of a location."""
+        if not isinstance(other, Real):
+            raise TypeError(_type_error_text(self, other, '*'))
+
+        if other == 0:
+            return Location()
+        else:
+            return Location(
+                self.offset * other,
+                frozenset((loc, c * other) for loc, c in self.references)
+            )
+
+    def __rmul__(self, other) -> 'Location':
+        """Scalar multiplication of a location."""
+        return self.__mul__(other)
 
 resolve_types_with_validation(Location, globals(), locals())
--- tests/sequencer/test_locations.py
+++ tests/sequencer/test_locations.py
@@ -1,4 +1,5 @@
 import pytest
+import attrs
 
 from qwip.sequencer.locations import Location
 
@@ -19,27 +20,72 @@ class TestLocation:
         l4 = Location(references={(l1, 0.5), (l2, 0.5), (l3, 0.5)})
         assert l4.references == frozenset({(l1, 0.5), (l2, 0.5), (l3, 0.5)})
 
-    def test_negation(self):
-        assert Location() == -Location()
-        assert Location(5) == -Location(-5)
-
-        l1 = Location(
-            references={(Location(1), 1), (Location(2), 0.5)}
-        )
-
-        l1neg = Location(
-            references={(Location(1), -1), (Location(2), -0.5)}
-        )
-        assert l1 == -l1neg
-        assert -l1 == l1neg
+    @pytest.mark.parametrize(
+        'location,inverse',
+        [
+            (Location(), Location()),
+            (Location(1), Location(-1)),
+            (Location(-1), Location(1)),
+            (Location(-1, {(Location(1), 1)}), Location(1, {(Location(1), -1)}))
+        ]
+    )
+    def test_negation(self, location, inverse):
+        assert -location == inverse
 
 
-    def test_add(self):
-        assert Location() + Location() == Location()
+    @pytest.mark.parametrize(
+        'op1,op2,result',
+        [
+            (Location(), Location(), Location()),
+            (Location(1), Location(-1), Location()),
+            (Location(), Location(5), Location(5)),
+            (Location(), 5, Location(5)),
+            (5, Location(), Location(5)),
+            ('5', Location(), pytest.raises(TypeError)),
+            (Location(1, {(Location(5), 1)}), Location(-1, {(Location(5), -1)}), Location())
+        ]
+    )
+    def test_add(self, op1, op2, result):
+        if hasattr(result, '__enter__'):
+            with result:
+                op1 + op2
+        else:
+            assert op1 + op2 == result
 
-        l1 = Location(
+    @pytest.mark.parametrize(
+        'op1,op2,result',
+        [
+            (Location(), Location(), Location()),
+            (Location(1), Location(1), Location()),
+            (Location(), Location(5), Location(-5)),
+            (Location(), 5, Location(-5)),
+            (5, Location(), Location(5)),
+            ('5', Location(), pytest.raises(TypeError)),
+            (Location(1, {(Location(5), 1)}), Location(1, {(Location(5), 1)}), Location())
+        ]
+    )
+    def test_subtract(self, op1, op2, result):
+        if hasattr(result, '__enter__'):
+            with result:
+                op1 - op2
+        else:
+            assert op1 - op2 == result
 
-        )
+    @pytest.mark.parametrize(
+        'op1,op2,result',
+        [
+            (Location(1), 0, Location()),
+            (0, Location(1, {(Location(5), 1)}), Location()),
+            (Location(1, {(Location(2), 1)}), 0.5, Location(0.5, {(Location(2), 0.5)})),
+            ('5', Location(), pytest.raises(TypeError)),
+        ]
+    )
+    def test_scalar_multiply(self, op1, op2, result):
+        if hasattr(result, '__enter__'):
+            with result:
+                op1 * op2
+        else:
+            assert op1 * op2 == result
 
     def test_repr(self):
         l0 = Location()
@@ -47,8 +93,3 @@ class TestLocation:
         l2 = Location(10)
 
         l3 = Location(1, references={(l1, 0.5), (l2, 1)})
-
-        print(l0)
-        print(l1)
-        print(l2)
-        print(l3)
