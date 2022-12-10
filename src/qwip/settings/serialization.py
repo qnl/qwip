39cef1303abc8b1778c204c1f04753c75ca8aeeb
Larry Chen <larrychen@berkeley.edu>
2022-12-10 00:18:10 -0800

    Replace get type converter with a callable class to save the type

--- src/qwip/settings/serialization.py
+++ src/qwip/settings/serialization.py
@@ -1,29 +1,39 @@
 """Serializers for settings."""
-
+from attrs import frozen
 from loguru import logger
 
 import qwip
+from qwip.typing import replace_self_type
 
-def get_type_converter(cls):
-    def convert(v):
-        """Attempts type structuring.
-        
-        We want most errors to be caught by validation since this provides a
-        clearer error message"""
+
+@frozen
+class _TypeConverter:
+    fieldtype: type
+
+    def __call__(self, v):
         try:
-            return qwip.converter.structure(v, cls)
-        except Exception as e:
+            return qwip.converter.structure(v, self.fieldtype)
+        except Exception as e:            
             logger.debug(f'Conversion failed due to {e}.')
             return v
-
-    return convert
 
 def add_type_converters(cls, fields):
     new_fields = []
 
     for field in fields:
-        if field.metadata.get('auto_convert', True) and field.init and field.converter is None:
-            field = field.evolve(converter=get_type_converter(field.type))
+        if (
+            field.metadata.get('auto_convert', True) and 
+            field.init and 
+            (
+                field.converter is None or
+                (
+                    isinstance(field.converter, _TypeConverter) and 
+                    cls != field.converter.fieldtype
+                )
+            )
+        ):
+            converter = _TypeConverter(replace_self_type(field.type, cls))
+            field = field.evolve(converter=converter)
 
         new_fields.append(field)
 
