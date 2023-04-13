2d50038677977f3a8e858ce52e3f0716bab64496
Larry Chen <larrychen@berkeley.edu>
2023-04-12 22:28:57 -0700

    Replace qtrl ReadoutInfo to minimize qtrl imports

--- src/qwip/sequencer/compilation.py
+++ src/qwip/sequencer/compilation.py
@@ -12,7 +12,6 @@ from loguru import logger
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from numpy.typing import NDArray
-from qtrl.sequence_utils.readout import _ReadoutInfo
 from scipy.fft import fft, fftfreq, fftshift
 from typing_extensions import Self
 
@@ -48,6 +47,23 @@ def find_readout_marker(locations) -> Location | None:
             readout_location = loc
 
     return readout_location
+
+
+@qdefine
+class _ReadoutInfo:
+    """Readout info.
+
+    This is a mirror of the _ReadoutInfo class in `qtrl.sequence_utils.readout`.
+
+    Attributes:
+        sequence: A WaveformData object to mirror a QTRL sequence.
+        qubits: A list of qubit indices corresponding to which qubits a read out.
+        n_readouts: The total number of readouts accross all sequence elements.
+    """
+
+    sequence: "WaveformData"
+    qubits: list[int]
+    n_readouts: int
 
 
 @qdefine
