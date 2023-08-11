5d0025040cc6c7d710e685e411eb77dca91845e0
Larry Chen <larrychen@berkeley.edu>
2023-08-11 14:26:18 -0700

    Move database tests to database package

--- tests/test_qwip/config/test_database.py
+++ tests/test_qwip/config/test_database.py
@@ -1,77 +1,8 @@
-import warnings
-
-import attrs
 import pendulum
 import pytest
-import sqlalchemy as sa
 
-from qwip.config.interface import ConfigDB, ConfigFolder, ReadOnlyParameter
-from qwip.config.models import Folder, Parameter
+from qwip.config.interface import ConfigFolder, ReadOnlyParameter
 from qwip.config.schema import ConfigSchema
-from qwip.database.database import Branch, Commit, Status
-from qwip.database.metadata import QWIP_DB_METADATA
-from qwip.testing import ignore_order
-
-
-class TestDatabase:
-    def test_tables(self, database, models):
-        assert database.tables() == {
-            "folders",
-            "parameters",
-            "waveforms",
-            "waveform_locations",
-            "constraints",
-            "sequence_elements",
-            "datastore_entries",
-        }
-
-
-@pytest.mark.usefixtures("skip_dolt")
-class TestDoltDB:
-    def test_current_branch(self, database):
-        branch = database.current_branch()
-        assert branch.name == "main"
-
-    def test_create_delete_branch(self, database):
-        database.branch("new")
-        branch = database.get_branch("new")
-        assert branch.name == "new"
-
-        try:
-            database.branch("new", action="delete")
-        except sa.exc.OperationalError:
-            warnings.warn("Forcing branch deletion")
-            database.branch("new", action="delete", force=True)
-
-        assert database.get_branch("new") is None
-
-    def test_checkout_branch(self, database):
-        database.branch("new")
-        new = database.get_branch("new")
-        current = database.checkout("new")
-
-        assert new == current
-        main = database.get_branch("main")
-        current = database.checkout("main")
-
-        assert main == current
-
-        try:
-            database.branch("new", action="delete")
-        except sa.exc.OperationalError:
-            warnings.warn("Forcing branch deletion")
-            database.branch("new", action="delete", force=True)
-
-    def test_status(self, database, models):
-        assert database.status() == ignore_order(
-            [
-                Status(table=t, staged=False, status="new table")
-                for t in database.tables()
-            ]
-        )
-
-    def test_author(self, database):
-        assert database.author == "pytest <pytest@qnl>"
 
 
 @pytest.fixture(scope="function")
