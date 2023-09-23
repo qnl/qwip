eb08da50284f789d08e762f0c6e8569dd4ec7b9b
Larry Chen <larrychen@berkeley.edu>
2023-09-22 23:23:04 -0700

    Fix failing tests on FakeBackend

--- src/qwip/backends/backend.py
+++ src/qwip/backends/backend.py
@@ -124,10 +124,10 @@ class FakeBackend(QuantumBackend):
 
         data = dict()
         for key in readout_keys:
-            states = np.zeros((num_elements, num_readouts, repetitions))
+            states = np.zeros((num_elements, repetitions, num_readouts))
             for el in range(num_elements):
                 for ro in range(num_readouts):
-                    states[el, ro, :] = self.data_func(key, el, ro, repetitions)
+                    states[el, :, ro] = self.data_func(key, el, ro, repetitions)
 
             gmm = self.gmms[key]
             arr = np.zeros(states.shape, dtype=np.complex64)
