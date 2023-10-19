90a0f3209a6f5875cef98ecedc7c4cc60ea04f19
Larry Chen <larrychen@berkeley.edu>
2023-10-18 17:48:10 -0700

    Only store sequence for now

--- src/qwip/qpu/qpu.py
+++ src/qwip/qpu/qpu.py
@@ -5,8 +5,6 @@ a superconduting quantum device as a quantum circuit processor, including circui
 compilation/transpilation, data acquisition, and measurement processing. This is the
 main user interface for interacting with experimental devices.
 """
-import re
-
 from attrs import field
 from loguru import logger
 
@@ -23,15 +21,14 @@ from qwip.processing.data_processor import (
     ReadoutPipeline,
 )
 from qwip.processing.processors import GMMClassification, IQRotation
-from qwip.qpu.systems import REGISTERED_QSYSTEMS, QuantumSystem, ReadoutResonator
-from qwip.sequencer import Sequence, SequenceElement
+from qwip.qpu.systems import REGISTERED_QSYSTEMS, QuantumSystem
+from qwip.sequencer import Sequence
 from qwip.sequencer.compilation import (
     REGISTERED_COMPILERS,
     ChannelInfo,
     DeviceInfo,
     QuantumExecutable,
     QWiPCompiler,
-    TriggerInfo,
 )
 from qwip.sequencer.phase_tracker import ModulationFrequency
 
@@ -312,7 +309,10 @@ class QPU:
 
         if self.datastore:
             data = dict(config_db=self.db) | data
-            self.datastore.save(*self.pipeline.grouped_data(), executable=exe, **data)
+            if exe and exe.sequence is not None:
+                data["sequence"] = exe.sequence
+
+            self.datastore.save(*self.pipeline.grouped_data(), **data)
 
         return processed
 
