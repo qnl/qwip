8dcb8d15f535415e18e3d3530d51bf577df54703
Larry Chen <larrychen@berkeley.edu>
2023-10-21 22:19:01 -0700

    Update deprecation timeline for plot_readout_IQ

--- src/qwip/visualization/readout.py
+++ src/qwip/visualization/readout.py
@@ -129,7 +129,7 @@ def plot_IQ_histogram(
 
 
 @deprecated(
-    version="23.2.0", removed="23.3.0", message="Use `plot_IQ_histogram` instead."
+    version="23.10.0", removed="23.12.0", message="Use `plot_IQ_histogram` instead."
 )
 def plot_readout_IQ(
     data: "IQResult",
@@ -144,7 +144,7 @@ def plot_readout_IQ(
     """Plots an IQResult as a 2d histogram.
 
     !!! Warning
-        Deprecated since version 23.2.0. `plot_readout_IQ` will be removed in 23.3.0.
+        Deprecated since version 23.10.0. `plot_readout_IQ` will be removed in 23.12.0.
 
     Args:
         data: The IQResult.
