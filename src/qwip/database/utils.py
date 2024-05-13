43cefdc048b48c88802b4eca437f0b9851062d4c
Larry Chen <larrychen@berkeley.edu>
2024-05-12 21:45:31 -0700

    Fix SQLAlchemy PendulumDateTime to use iso8601 when saving to database

--- src/qwip/database/utils.py
+++ src/qwip/database/utils.py
@@ -44,6 +44,10 @@ class PendulumDateTime(types.TypeDecorator):
     impl = types.DateTime
     cache_ok = True
 
+    def process_bind_param(self, value, dialect):
+        # pendulum 3.0 default repr is no longer ISO8601 compatible.
+        return value.isoformat()
+
     def process_result_value(self, value, dialect):
         return pendulum.instance(value).in_tz("local")
 
