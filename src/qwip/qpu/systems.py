3e6b25b29ffb28954e01d16ff65a2beb6a07ab70
Larry Chen <larrychen@berkeley.edu>
2024-11-20 11:17:10 -0800

    Add parameter fitting to transmon

--- src/qwip/qpu/systems.py
+++ src/qwip/qpu/systems.py
@@ -84,6 +84,50 @@ class Transmon(QuantumSystem):
         name = self.frame_key.format(name=self.name, subspace=subspace)
         return self.get_frames()[name]
 
+    @staticmethod
+    def compute_transmon_parameters(omega, alpha, tolerance=1e-5):
+        """Computes Ej and Ec given the frequency and anharmonicity of the qubit.
+
+        This function uses the fourth order taylor expansion of $\\omega$ and 
+        $\\alpha$ to solve $E_C$ and $E_J$. The taylor expansions are in terms
+        of the small parameter $\\eta = \\sqrt{\\frac{2E_C}{E_J}}$, as given in
+        [1]. 
+        
+        The units of omega and alpha, in principle, do not matter as long as
+        they are consistent. However, working in units of GHz tends to give
+        better results.
+
+        Args:
+            omega (float): The frequency (01) of the transmon. See above on units.
+            alpha (float): The anharmonicity of the transmon, defined as f12 - f01. 
+                This should be a negative number. See above on units.
+
+        Returns:
+            tuple: A tuple (Ec, Ej) with the same units as omega and alpha.
+
+        References:
+            [1]: See https://arxiv.org/abs/1706.06566.
+        """
+        p_a = np.poly1d([46899/(2**15), 4635/(2**12), 81/(2**7), 9/(2**4), 1])
+        p_w = np.poly1d([-5319/(2**15), -19/(2**7), -21/(2**7), -1/4, -1, 4])
+
+        eta = np.poly1d([1, 0])
+
+        p_t = eta*omega*p_a + alpha*p_w
+
+        roots = p_t.r[-1]
+
+        eta_0 = np.real(roots[np.isreal(roots)][0])
+
+        if not np.isclose(p_t(eta_0), 0, atol=tolerance):
+            logger.warning("Error: Unable to find a solution.")
+
+        Ec = -alpha/p_a(eta_0)
+
+        Ej = 2*Ec/(eta_0**2)
+
+        return Ec, Ej
+
 
 @register_qsystem
 @qdefine
